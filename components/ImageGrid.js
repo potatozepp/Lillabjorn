47bb2ba06d0a5cf155ddde69de1df5803fdf936e
Sebastian Oberladstaetter <sebastian.oberladstaetter@storetec-systems.com>
2025-07-22 08:45:41 +0200

    update images

--- components/ImageGrid.js
+++ components/ImageGrid.js
@@ -2,10 +2,16 @@ import Image from 'next/image'
 
 export default function ImageGrid({ images }) {
   return (
-    <div className="cards">
-      {images.map((img, idx) => (
+      <div className="cards">
+      {images.map((image, idx) => (
         <div key={idx} className="card">
-          <Image src={img.src} alt={img.alt} width={300} height={200} layout="responsive" />
+          <Image
+            src={image.src}
+            alt={image.alt}
+            width={300}
+            height={200}
+            sizes="(max-width: 600px) 100vw, 33vw"
+          />
         </div>
       ))}
     </div>
